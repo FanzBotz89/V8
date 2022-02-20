e837d4f29fbceedaaa3f138f67e0afb283655e9e
FanzBotz89 <faisalalbono@gmail.com>
2022-02-20 15:03:59 +0700

    selfbot-master

---
+++ message/sewabot.js
@@ -0,0 +1,52 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* 2 hari = 2k         
+*│▸* 7 hari = 5k         
+*│▸* permanen = 10k                          
+*┬*
+*╰────────────[*
+
+*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*┴*
+*│▸* Shopee : ${settings.Shopee}
+*│▸* Gopay : ${settings.Gopay}
+*│▸* Dana : ${settings.Dana}
+*│▸* Ovo : ${settings.Ovo}
+*┬*
+*╰────────────[*
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang 
+juga mati klo lgi ada error 
+atau lgi perbaikan bug.
+
+Kalo mau sewa bisa chat
+owner Bot,
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
